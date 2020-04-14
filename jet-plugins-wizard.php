c6c0b927ba37aa112b2fa3eaa900a942a5bd4325
ZemezLab <33900119+ZemezLab@users.noreply.github.com>
2020-04-14 12:57:33 +0300

    Update jet-plugins-wizard.php

--- jet-plugins-wizard.php
+++ jet-plugins-wizard.php
@@ -3,7 +3,7 @@
  * Plugin Name: Jet Plugins Wizard
  * Plugin URI:  https://zemez.io
  * Description: Plugins installation wizard.
- * Version:     1.2.5
+ * Version:     1.2.6
  * Author:      Zemez
  * Author URI:
  * Text Domain: jet-plugins-wizard
