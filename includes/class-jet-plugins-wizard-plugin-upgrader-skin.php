a33ae8fa0d466ba340a151b8b42f87aeccd8005f
Dmitriy Bartoshchak <kaskad.8888@gmail.com>
2021-09-23 11:42:12 +0300

    FIX: compatibility with PHP8

--- includes/class-jet-plugins-wizard-plugin-upgrader-skin.php
+++ includes/class-jet-plugins-wizard-plugin-upgrader-skin.php
@@ -108,8 +108,9 @@ class Jet_Plugins_Wizard_Plugin_Upgrader_Skin extends Plugin_Installer_Skin {
 	/**
 	 *
 	 * @param string $string
+	 * @param mixed  ...$args
 	 */
-	public function feedback( $string ) {
+	public function feedback( $string, ...$args ) {
 
 		if ( isset( $this->upgrader->strings[ $string ] ) )
 			$string = $this->upgrader->strings[ $string ];
